f0c8e604a2880540d83f83dc89a5aec5af99ffc8
Edwin K. Orioki <oriokie@icloud.com>
2024-06-24 05:41:50 -0400

    :bug: removed the comments

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // eslint-disable-line no-unused-vars
-    let task2 = false; // eslint-disable-line no-unused-vars
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
