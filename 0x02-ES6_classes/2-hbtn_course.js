9084954e9456d640e4a4aae5974df856618c93d1
Edwin <oriokie@icloud.com>
2024-06-27 23:35:38 +0300

    📖 2. A Course, Getters, and Setters done

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,52 @@
+// Implementing a class name HolbertonCourse
+export default class HolbertonCourse {
+  /**
+   * creates a new course
+   * @param {string} name - The name of the course
+   * @param {number} length - The length of the course
+   * @param {string} students - The students in the course
+   */
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  // Getters
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  // Setters
+  set name(newName) {
+    if (typeof newName !== "string") {
+      throw new TypeError("Name must be a string");
+    }
+    this._name = newName;
+  }
+
+  set length(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("Length must be a number");
+    }
+    this._length = value;
+  }
+
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError("Students must be an array of strings");
+    }
+    if (!value.every((student) => typeof student === "string")) {
+      throw new TypeError("Students must be an array of strings");
+    }
+    this._students = value;
+  }
+}
