96c04ee343097c19c99e2e138409ae9f6f60d3d4
Edwin <oriokie@icloud.com>
2024-08-02 12:14:41 +0300

    end point task

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,16 @@
+export const weakMap = new WeakMap();
+
+const MAX_NO_OF_CALLS = 5;
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+
+  const count = weakMap.get(endpoint) + 1;
+  weakMap.set(endpoint, count);
+
+  if (count >= MAX_NO_OF_CALLS) {
+    throw new Error('Endpoint load is high');
+  }
+}
